58a396556355e6698f92721dfd6e87acf3e3f32c
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-05-03 14:56:05 +0300

    Creates function return markup trip item

--- src/bundle.js
+++ src/bundle.js
@@ -270,3 +270,14 @@ const createTripListComponent = () => {
     `<ul class="trip-days"></ul>`
   );
 };
+
+const createTripItemComponent = () => {
+  return (
+    `<li class="trip-days__item  day">
+      <div class="day__info">
+        <span class="day__counter">1</span>
+        <time class="day__date" datetime="2019-03-18">MAR 18</time>
+      </div>
+    </li>`
+  );
+};
