07a86699de00ff666105f3e8f203e6ef2d4c381a
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-05-03 14:52:49 +0300

    Creates function return markup navigation menu

--- src/bundle.js
+++ src/bundle.js
@@ -18,3 +18,12 @@ const createTripInfoComponent = () => {
     </section>`
   );
 };
+
+const createNavMenuComponent = () => {
+  return (
+    `<nav class="trip-controls__trip-tabs  trip-tabs">
+      <a class="trip-tabs__btn  trip-tabs__btn--active" href="#">Table</a>
+      <a class="trip-tabs__btn" href="#">Stats</a>
+    </nav>`
+  );
+};
