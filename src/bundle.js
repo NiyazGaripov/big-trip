a9b5e685474e76b03ee988acb9432d40f311c968
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-05-03 14:50:49 +0300

    Finds DOM elements

--- src/bundle.js
+++ src/bundle.js
@@ -1,1 +1,4 @@
 const TRIP_EVENT_COUNT = 3;
+const tripMain = document.querySelector(`.trip-main`);
+const tripMainControls = tripMain.querySelector(`.trip-main__trip-controls`);
+const tripEvents = document.querySelector(`.trip-events`);
