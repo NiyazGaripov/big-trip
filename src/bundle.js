53240dcde8802f12fc542620ba180a74837d4fce
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-05-03 14:52:07 +0300

    Creates function return markup trip info

--- src/bundle.js
+++ src/bundle.js
@@ -2,3 +2,19 @@ const TRIP_EVENT_COUNT = 3;
 const tripMain = document.querySelector(`.trip-main`);
 const tripMainControls = tripMain.querySelector(`.trip-main__trip-controls`);
 const tripEvents = document.querySelector(`.trip-events`);
+
+const createTripInfoComponent = () => {
+  return (
+    `<section class="trip-main__trip-info  trip-info">
+      <div class="trip-info__main">
+        <h1 class="trip-info__title">Amsterdam &mdash; Chamonix &mdash; Geneva</h1>
+
+        <p class="trip-info__dates">Mar 18&nbsp;&mdash;&nbsp;20</p>
+      </div>
+
+      <p class="trip-info__cost">
+        Total: &euro;&nbsp;<span class="trip-info__cost-value">1230</span>
+      </p>
+    </section>`
+  );
+};
