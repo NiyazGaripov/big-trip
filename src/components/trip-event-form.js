7f294b9cca4cf1afdf88668f86cefadce4c3b4e2
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-08-13 19:31:05 +0300

    Removes static marup and generates transfers field group

--- src/components/trip-event-form.js
+++ src/components/trip-event-form.js
@@ -1,52 +1,23 @@
 const TRANSFERS = [`taxi`, `bus`, `train`, `ship`, `transport`, `drive`, `flight`];
 
-const createEventTypeItemMarkup = (item) => {
+const createEventTypeItemMarkup = (item, isChecked) => {
   return (
     `<div class="event__type-item">
-      <input id="event-type-${item}-1" class="event__type-input  visually-hidden" type="radio" name="event-type" value="${item}">
+      <input id="event-type-${item}-1" class="event__type-input  visually-hidden" type="radio" name="event-type" value="${item}" ${isChecked ? `checked` : ``}>
       <label class="event__type-label  event__type-label--${item}" for="event-type-${item}-1">${item}</label>
     </div>`
   );
 };
 
 const createEventTypeGroupMarkup = () => {
-  const eventTypeItemMarkup = createEventTypeItemMarkup(`taxi`);
+  const eventTypeItemsMarkup = TRANSFERS.map((it, index) => createEventTypeItemMarkup(it, index === 0)).join(`\n`);
 
   return (
     `<fieldset class="event__type-group">
       <legend class="visually-hidden">Transfer</legend>
 
-      ${eventTypeItemMarkup}
+      ${eventTypeItemsMarkup}
 
-      <div class="event__type-item">
-        <input id="event-type-bus-1" class="event__type-input  visually-hidden" type="radio" name="event-type" value="bus">
-        <label class="event__type-label  event__type-label--bus" for="event-type-bus-1">Bus</label>
-      </div>
-
-      <div class="event__type-item">
-        <input id="event-type-train-1" class="event__type-input  visually-hidden" type="radio" name="event-type" value="train">
-        <label class="event__type-label  event__type-label--train" for="event-type-train-1">Train</label>
-      </div>
-
-      <div class="event__type-item">
-        <input id="event-type-ship-1" class="event__type-input  visually-hidden" type="radio" name="event-type" value="ship">
-        <label class="event__type-label  event__type-label--ship" for="event-type-ship-1">Ship</label>
-      </div>
-
-      <div class="event__type-item">
-        <input id="event-type-transport-1" class="event__type-input  visually-hidden" type="radio" name="event-type" value="transport">
-        <label class="event__type-label  event__type-label--transport" for="event-type-transport-1">Transport</label>
-      </div>
-
-      <div class="event__type-item">
-        <input id="event-type-drive-1" class="event__type-input  visually-hidden" type="radio" name="event-type" value="drive">
-        <label class="event__type-label  event__type-label--drive" for="event-type-drive-1">Drive</label>
-      </div>
-
-      <div class="event__type-item">
-        <input id="event-type-flight-1" class="event__type-input  visually-hidden" type="radio" name="event-type" value="flight" checked>
-        <label class="event__type-label  event__type-label--flight" for="event-type-flight-1">Flight</label>
-      </div>
     </fieldset>`
   );
 };
