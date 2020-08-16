9f2dba5f6d10b98a9239f7053dadbb8a279ae83b
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-08-16 23:01:33 +0300

    Replaces markup to func creates markup

--- src/components/trip-event-form.js
+++ src/components/trip-event-form.js
@@ -59,12 +59,12 @@ const createDestinationMarkup = (destination) => {
   );
 };
 
-const createOfferMarkup = (item) => {
+const createOfferMarkup = (item, isChecked) => {
   const {offer, title, price} = item;
 
   return (
     `<div class="event__offer-selector">
-      <input class="event__offer-checkbox  visually-hidden" id="event-offer-${offer}-1" type="checkbox" name="event-offer-${offer}" checked>
+      <input class="event__offer-checkbox  visually-hidden" id="event-offer-${offer}-1" type="checkbox" name="event-offer-${offer}" ${isChecked ? `checked` : ``}>
       <label class="event__offer-label" for="event-offer-${offer}-1">
         <span class="event__offer-title">${title}</span>
         &plus;
@@ -78,6 +78,7 @@ export const createTripEventFormComponent = () => {
   const eventTypeTransfers = createEventTypeGroupMarkup(TRANSFERS, `Transfer`);
   const eventTypeActivities = createEventTypeGroupMarkup(ACTIVITIES, `Activity`);
   const destinations = CITIES.map((it) => createDestinationMarkup(it));
+  const eventOffers = OFFERS.map((it, index) => createOfferMarkup(it, index === 0)).join(`\n`);
 
   return (
     `<form class="trip-events__item  event  event--edit" action="#" method="post">
@@ -134,51 +135,7 @@ export const createTripEventFormComponent = () => {
           <h3 class="event__section-title  event__section-title--offers">Offers</h3>
 
           <div class="event__available-offers">
-            <div class="event__offer-selector">
-              <input class="event__offer-checkbox  visually-hidden" id="event-offer-luggage-1" type="checkbox" name="event-offer-luggage" checked>
-              <label class="event__offer-label" for="event-offer-luggage-1">
-                <span class="event__offer-title">Add luggage</span>
-                &plus;
-                &euro;&nbsp;<span class="event__offer-price">30</span>
-              </label>
-            </div>
-
-            <div class="event__offer-selector">
-              <input class="event__offer-checkbox  visually-hidden" id="event-offer-comfort-1" type="checkbox" name="event-offer-comfort" checked>
-              <label class="event__offer-label" for="event-offer-comfort-1">
-                <span class="event__offer-title">Switch to comfort class</span>
-                &plus;
-                &euro;&nbsp;<span class="event__offer-price">100</span>
-              </label>
-            </div>
-
-            <div class="event__offer-selector">
-              <input class="event__offer-checkbox  visually-hidden" id="event-offer-meal-1" type="checkbox" name="event-offer-meal">
-              <label class="event__offer-label" for="event-offer-meal-1">
-                <span class="event__offer-title">Add meal</span>
-                &plus;
-                &euro;&nbsp;<span class="event__offer-price">15</span>
-              </label>
-            </div>
-
-            <div class="event__offer-selector">
-              <input class="event__offer-checkbox  visually-hidden" id="event-offer-seats-1" type="checkbox" name="event-offer-seats">
-              <label class="event__offer-label" for="event-offer-seats-1">
-                <span class="event__offer-title">Choose seats</span>
-                &plus;
-                &euro;&nbsp;<span class="event__offer-price">5</span>
-              </label>
-            </div>
-
-            <div class="event__offer-selector">
-              <input class="event__offer-checkbox  visually-hidden" id="event-offer-train-1" type="checkbox" name="event-offer-train">
-              <label class="event__offer-label" for="event-offer-train-1">
-                <span class="event__offer-title">Travel by train</span>
-                &plus;
-                &euro;&nbsp;<span class="event__offer-price">40</span>
-              </label>
-            </div>
-          </div>
+            ${eventOffers}
         </section>
 
         <section class="event__section  event__section--destination">
