427c0d2b3b57c85b8d520d6fe347ddb3760ec165
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-09-14 23:14:26 +0300

    Adds isChecked

--- src/components/filter.js
+++ src/components/filter.js
@@ -1,16 +1,16 @@
 const FILTERS = [`everything`, `future`, `past`];
 
-const createTripFilter = (filter) => {
+const createTripFilter = (filter, isChecked) => {
   return (
     `<div class="trip-filters__filter">
-      <input id="filter-${filter}" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="${filter}" checked>
+      <input id="filter-${filter}" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="${filter}" ${isChecked ? `checked` : ``}>
       <label class="trip-filters__filter-label" for="filter-${filter}">${filter}</label>
     </div>`
   );
 };
 
 export const createFilterComponent = () => {
-  const createFilters = FILTERS.map((filter) => createTripFilter(filter)).join(`\n`);
+  const createFilters = FILTERS.map((filter, index) => createTripFilter(filter, index === 0)).join(`\n`);
 
   return (
     `<form class="trip-filters" action="#" method="get">
