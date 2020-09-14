ed7b19f0e99398953aaced9c75e6e9f4a5644fc8
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-09-14 23:27:31 +0300

    Adds sort list

--- src/components/sort.js
+++ src/components/sort.js
@@ -23,31 +23,13 @@ const createSortList = (item, isChecked) => {
 };
 
 export const createSortComponent = () => {
+  const sortList = NAMES.map((item, index) => createSortList(item, index === 0)).join(`\n`);
 
   return (
     `<form class="trip-events__trip-sort  trip-sort" action="#" method="get">
       <span class="trip-sort__item  trip-sort__item--day">Day</span>
 
-      <div class="trip-sort__item  trip-sort__item--event">
-        <input id="sort-event" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-event" checked>
-        <label class="trip-sort__btn" for="sort-event">Event</label>
-      </div>
-
-      <div class="trip-sort__item  trip-sort__item--time">
-        <input id="sort-time" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-time">
-        <label class="trip-sort__btn" for="sort-time">
-          Time
-          ${icon}
-        </label>
-      </div>
-
-      <div class="trip-sort__item  trip-sort__item--price">
-        <input id="sort-price" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-price">
-        <label class="trip-sort__btn" for="sort-price">
-          Price
-          ${icon}
-        </label>
-      </div>
+      ${sortList}
 
       <span class="trip-sort__item  trip-sort__item--offers">Offers</span>
     </form>`
