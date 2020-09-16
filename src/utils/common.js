f644e66277ff1af54256336f180874057ae53670
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-09-16 21:32:55 +0300

    Creates func getRandomArrayItem

--- src/utils/common.js
+++ src/utils/common.js
@@ -1,3 +1,9 @@
 export const getRandomIntegerNumber = (min, max) => {
   return min + Math.floor(Math.random() * (max - min));
 };
+
+export const getRandomArrayItem = (array) => {
+  const randomItem = getRandomIntegerNumber(0, array.length);
+
+  return array[randomItem];
+};
