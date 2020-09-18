1d6332aff89d1e35e78a2fa091efb2bb76ce4106
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-09-18 23:07:42 +0300

    Creates func generateEventCard

--- src/mock/event.js
+++ src/mock/event.js
@@ -6,3 +6,22 @@ const MIN_OFFERS_AMOUNT = 0;
 const MAX_OFFERS_AMOUNT = 5;
 const MIN_AMOUNT = 1;
 const MAX_AMOUNT = 5;
+
+const generateEventCard = () => {
+  const eventType = getRandomArrayItem(TRANSFERS);
+  const city = getRandomArrayItem(CITIES);
+  const cost = Math.floor(Math.random() * COST_COEFFICIENT);
+  const offersAmount = getRandomIntegerNumber(MIN_OFFERS_AMOUNT, MAX_OFFERS_AMOUNT);
+  const offers = generateOffers(offersAmount);
+  const description = generateDescription(DESCRIPTIONS, MIN_AMOUNT, MAX_AMOUNT);
+  const photos = new Array(getRandomIntegerNumber(MIN_AMOUNT, MAX_AMOUNT)).fill(``).map(() => `http://picsum.photos/248/152?r=${Math.random()}`);
+
+  return {
+    eventType,
+    city,
+    cost,
+    offers,
+    description,
+    photos,
+  };
+};
