d0bee4384c43f30a371e065cf45d83b323cd0b88
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-09-18 23:06:25 +0300

    Imports dependences

---
+++ src/mock/event.js
@@ -0,0 +1,3 @@
+import {TRANSFERS, CITIES, DESCRIPTIONS, COST_COEFFICIENT} from './../constants.js';
+import {getRandomIntegerNumber, getRandomArrayItem, generateDescription} from './../utils/common.js';
+import {generateOffers} from './../mock/offers.js';
