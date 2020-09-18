c5994e899542d3da59eff20bdfb6e271d0c9f646
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-09-18 10:07:47 +0300

    Corrects offers

--- src/constants.js
+++ src/constants.js
@@ -44,26 +44,11 @@ export const CITIES = [
 ];
 
 export const OFFERS = [
-  {
-    title: `Add luggage`,
-    price: 30,
-  },
-  {
-    title: `Switch to comfort class`,
-    price: 100,
-  },
-  {
-    title: `Add meal`,
-    price: 15,
-  },
-  {
-    title: `Choose seats`,
-    price: 5,
-  },
-  {
-    title: `Travel by train`,
-    price: 40,
-  },
+  `Add luggage`,
+  `Switch to comfort class`,
+  `Add meal`,
+  `Choose seats`,
+  `Travel by train`,
 ];
 
 export const DESCRIPTIONS = [
