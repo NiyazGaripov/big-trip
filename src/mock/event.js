c3214f13705e4624d705a1cabd1178cd25fbf785
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-09-18 23:07:13 +0300

    Adds constants

--- src/mock/event.js
+++ src/mock/event.js
@@ -1,3 +1,8 @@
 import {TRANSFERS, CITIES, DESCRIPTIONS, COST_COEFFICIENT} from './../constants.js';
 import {getRandomIntegerNumber, getRandomArrayItem, generateDescription} from './../utils/common.js';
 import {generateOffers} from './../mock/offers.js';
+
+const MIN_OFFERS_AMOUNT = 0;
+const MAX_OFFERS_AMOUNT = 5;
+const MIN_AMOUNT = 1;
+const MAX_AMOUNT = 5;
