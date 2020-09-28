7f696863c6c7b7e03d8418443ae1e6a6b137a23b
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-09-28 20:45:37 +0300

    Refactors mock event

--- src/mock/event.js
+++ src/mock/event.js
@@ -1,4 +1,4 @@
-import {TRANSFERS, ACTIVITIES, CITIES, DESCRIPTIONS, COST_COEFFICIENT} from './../constants.js';
+import {EVENT_GROUPS, CITIES, DESCRIPTIONS, COST_COEFFICIENT} from './../constants.js';
 import {getRandomIntegerNumber, getRandomArrayItem, generateDescription} from './../utils/common.js';
 import {generateOffers} from './../mock/offers.js';
 
@@ -6,10 +6,9 @@ const MIN_OFFERS_AMOUNT = 0;
 const MAX_OFFERS_AMOUNT = 5;
 const MIN_AMOUNT = 1;
 const MAX_AMOUNT = 5;
-const eventGroups = [...TRANSFERS, ...ACTIVITIES];
 
 const generateEventCard = () => {
-  const eventType = getRandomArrayItem(eventGroups);
+  const eventType = getRandomArrayItem(EVENT_GROUPS);
   const city = getRandomArrayItem(CITIES);
   const cost = Math.floor(Math.random() * COST_COEFFICIENT);
   const offersAmount = getRandomIntegerNumber(MIN_OFFERS_AMOUNT, MAX_OFFERS_AMOUNT);
