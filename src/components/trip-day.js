7ded9e3e2e671e1eeab6470bd914eb51b1f83589
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-09-29 22:14:42 +0300

    Adds evanet date

--- src/components/trip-day.js
+++ src/components/trip-day.js
@@ -1,9 +1,14 @@
+import {MONTH_NAMES} from './../constants.js';
+import {getRandomDate} from './../utils/common.js';
+
 export const createTripDayComponent = () => {
+  const date = `${MONTH_NAMES[getRandomDate().getMonth()]} ${getRandomDate().getDate()} `;
+
   return (
     `<li class="trip-days__item  day">
       <div class="day__info">
         <span class="day__counter">1</span>
-        <time class="day__date" datetime="2019-03-18">MAR 18</time>
+        <time class="day__date" datetime="2019-03-18">${date}</time>
       </div>
     </li>`
   );
