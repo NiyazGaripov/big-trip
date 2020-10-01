42a25a316c63db952e762dead0a818beadd8a57d
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-10-01 22:00:52 +0300

    Adds price

--- src/mock/event.js
+++ src/mock/event.js
@@ -2,12 +2,15 @@ import {EVENT_GROUPS, CITIES, DESCRIPTIONS, COST_COEFFICIENT} from './../constan
 import {getRandomIntegerNumber, getRandomArrayItem, generateDescription, getRandomDate} from './../utils/common.js';
 import {generateOffers} from './../mock/offers.js';
 
+const MIN_PRICE = 1000;
+const MAX_PRICE = 500000;
 const MIN_OFFERS_AMOUNT = 0;
 const MAX_OFFERS_AMOUNT = 5;
 const MIN_AMOUNT = 1;
 const MAX_AMOUNT = 5;
 
 const generateEventCard = () => {
+  const price = getRandomIntegerNumber(MIN_PRICE, MAX_PRICE);
   const eventType = getRandomArrayItem(EVENT_GROUPS);
   const city = getRandomArrayItem(CITIES);
   const date = getRandomDate();
@@ -18,6 +21,7 @@ const generateEventCard = () => {
   const photos = new Array(getRandomIntegerNumber(MIN_AMOUNT, MAX_AMOUNT)).fill(``).map(() => `http://picsum.photos/248/152?r=${Math.random()}`);
 
   return {
+    price,
     eventType,
     city,
     date,
