5853535510fcd491f17890d87a78a70e9cf23b41
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-10-01 21:51:37 +0300

    Adds trip event list

--- src/components/trip-day.js
+++ src/components/trip-day.js
@@ -10,6 +10,7 @@ export const createTripDayComponent = () => {
         <span class="day__counter">1</span>
         <time class="day__date" datetime="2019-03-18">${date}</time>
       </div>
+      <ul class="trip-events__list"></ul>
     </li>`
   );
 };
