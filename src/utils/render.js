1ca37c4a24f1986d853b2018493914f787d28ea9
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-10-21 20:21:25 +0300

    feat: creates func createElement

--- src/utils/render.js
+++ src/utils/render.js
@@ -1,3 +1,10 @@
 export const renderComponent = (container, component, place = `beforeend`) => {
   container.insertAdjacentHTML(place, component);
 };
+
+export const createElement = (template) => {
+  const newElement = document.createElement(`div`);
+  newElement.innerHTML = template;
+
+  return newElement.firstChild;
+};
