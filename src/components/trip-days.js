fd9e08fa3b752901712f8c3d472961b32563cbb7
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-10-21 19:38:39 +0300

    feat: addds param and creates sort events

--- src/components/trip-days.js
+++ src/components/trip-days.js
@@ -1,5 +1,10 @@
-export const createTripDaysComponent = () => {
+
+export const createTripDaysComponent = (events) => {
+  const sortEvents = events.slice().sort((a, b) => a.date - b.date);
+
   return (
-    `<ul class="trip-days"></ul>`
+    `<ul class="trip-days">
+        ${sortEvents.map((it, index) => createTripDayComponent(it, index)).join(`\n`)}
+    </ul>`
   );
 };
