df967eaaeaf88412d85bc3471a00176814c3cc78
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-10-21 19:39:15 +0300

    feat: creates func createTripDayComponent

--- src/components/trip-days.js
+++ src/components/trip-days.js
@@ -1,3 +1,22 @@
+import {MONTH_NAMES} from "../constants";
+import {createTripEventsItemComponent} from "./event";
+
+const createTripDayComponent = (item, index) => {
+  const tripEvent = createTripEventsItemComponent(item);
+
+  return (
+    `<li class="trip-days__item  day">
+      <div class="day__info">
+        <span class="day__counter">${index + 1}</span>
+        <time class="day__date" datetime="2019-03-18">${MONTH_NAMES[item.date.getMonth()]} ${item.date.getDate()}</time>
+      </div>
+      <ul class="trip-events__list">
+          ${tripEvent}
+      </ul>
+    </li>`
+  );
+};
+
 
 export const createTripDaysComponent = (events) => {
   const sortEvents = events.slice().sort((a, b) => a.date - b.date);
