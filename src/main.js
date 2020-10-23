fd8683a7e9159f194f5bb3055420d63a78e2d15b
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-10-23 18:56:46 +0300

    feat: creates instance TripInfo class and render

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import {renderComponent} from './utils/render.js';
-
+import {TripInfo} from './components/trip-info.js';
 import {generateNavigationList} from './mock/nav-menu.js';
 import {generateFilter} from './mock/filter.js';
 import {generateSortList} from './mock/sort.js';
@@ -14,3 +14,4 @@ const filters = generateFilter();
 const sortList = generateSortList();
 const events = generateEventCards(TRIP_EVENT_COUNT);
 
+renderComponent(tripMain, new TripInfo(events).getElement());
