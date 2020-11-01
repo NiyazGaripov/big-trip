9d5b39f66c19e1ab14afe2fedaa2cbd92967e6e1
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-11-01 22:32:17 +0300

    feat: creates module no events and func createNoEventsComponent

---
+++ src/components/no-events.js
@@ -0,0 +1,7 @@
+import {createElement} from './../utils/render.js';
+
+const createNoEventsComponent = () => {
+  return (
+    `<p class="trip-events__msg">Click New Event to create your first point</p>`
+  );
+};
