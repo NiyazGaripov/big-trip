bfc55e60b20d01438bce25561fdc2a3bb89f28fb
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-11-01 22:32:49 +0300

    feat: creates class NoEvents

--- src/components/no-events.js
+++ src/components/no-events.js
@@ -5,3 +5,25 @@ const createNoEventsComponent = () => {
     `<p class="trip-events__msg">Click New Event to create your first point</p>`
   );
 };
+
+export class NoEvents {
+  constructor() {
+    this._element = null;
+  }
+
+  getTemplate() {
+    return createNoEventsComponent();
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this.getTemplate());
+    }
+
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+}
