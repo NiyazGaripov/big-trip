59140c2f5acf797096e6068b7b1604fbcaf8e285
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-11-03 19:18:17 +0300

    Creates module and class abstract component

---
+++ src/components/abstract-component.js
@@ -0,0 +1,7 @@
+export class AbstractComponent {
+  constructor() {
+    if (new.target === AbstractComponent) {
+      throw new Error(`Can't instantiate AbstractComponent, only concrete one.`);
+    }
+  }
+}
