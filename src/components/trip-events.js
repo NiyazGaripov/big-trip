fab1a19a772b4c97f6d4c6c235be44d5a5e7e4fe
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-11-14 15:02:42 +0300

    feat: creates func createTripEventsComponent

--- src/components/trip-events.js
+++ src/components/trip-events.js
@@ -1,5 +1,13 @@
 import {AbstractComponent} from "./abstract-component";
 
+const createTripEventsComponent = () => {
+  return (
+    `<section class="trip-events">
+      <h2 class="visually-hidden">Trip events</h2>
+    </section>`
+  );
+};
+
 export class TripEvents extends AbstractComponent {
   constructor() {
     super();
