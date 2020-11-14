545195aa148dcad0189f415577e5ee1c67fa9d77
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-11-14 15:09:31 +0300

    refactor: transfers logic for drawing trip

--- src/controllers/trip.js
+++ src/controllers/trip.js
@@ -1,5 +1,63 @@
-export class TripController {
-  constructor() {}
+import {onEscKeyDown} from "../utils/common";
+import {TripEvent} from "../components/event";
+import {TripEventForm} from "../components/trip-event-form";
+import {renderComponent} from "../utils/render";
+import {NoEvents} from "../components/no-events";
+import {TripDays} from "../components/trip-days";
 
-  render() {}
+const renderTrip = (parentNode, it) => {
+  const replaceRoutePointToEventForm = () => {
+    parentNode.replaceChild(tripEventFormComponent.getElement(), tripEventComponent.getElement());
+  };
+
+  const replaceEventFormToRoutePoint = () => {
+    parentNode.replaceChild(tripEventComponent.getElement(), tripEventFormComponent.getElement());
+  };
+
+  const onFormCloseEsc = (evt) => {
+    onEscKeyDown(evt, replaceEventFormToRoutePoint);
+    document.removeEventListener(`keydown`, onFormCloseEsc);
+  };
+
+  const tripEventComponent = new TripEvent(it);
+
+  tripEventComponent.setClickHandler(() => {
+    replaceRoutePointToEventForm();
+    document.addEventListener(`keydown`, onFormCloseEsc);
+  });
+
+  const tripEventFormComponent = new TripEventForm(it);
+
+  tripEventFormComponent.setSubmitHandler(() => {
+    replaceEventFormToRoutePoint();
+    document.removeEventListener(`keydown`, onFormCloseEsc);
+  });
+
+  renderComponent(parentNode, tripEventComponent);
+};
+
+export class TripController {
+  constructor(container) {
+    this._container = container;
+    this._noEvents = new NoEvents();
+  }
+
+  render(events) {
+    const container = this._container;
+    const hasEvents = events.length > 0;
+
+    if (!hasEvents) {
+      renderComponent(container, this._noEvents);
+    }
+
+    renderComponent(container, new TripDays(events));
+
+    const tripEventsList = container.querySelectorAll(`.trip-events__list`);
+
+    events.forEach((it) => {
+      tripEventsList.forEach((event) => {
+        renderTrip(event, it);
+      });
+    });
+  }
 }
