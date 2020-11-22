69afaf1b3af2f9c1f2ba0fe972e183f54de038e7
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-11-22 20:11:03 +0300

    refactor: adds sort component

--- src/controllers/trip.js
+++ src/controllers/trip.js
@@ -4,6 +4,7 @@ import {TripEventForm} from "../components/trip-event-form";
 import {renderComponent} from "../utils/render";
 import {NoEvents} from "../components/no-events";
 import {TripDays} from "../components/trip-days";
+import {Sort} from './../components/sort';
 
 const renderTrip = (parentNode, it) => {
   const replaceRoutePointToEventForm = () => {
@@ -40,6 +41,7 @@ export class TripController {
   constructor(container) {
     this._container = container;
     this._noEvents = new NoEvents();
+    this._sortComponent = new Sort();
   }
 
   render(events) {
@@ -50,6 +52,7 @@ export class TripController {
       renderComponent(container, this._noEvents);
     }
 
+    renderComponent(container, this._sortComponent);
     renderComponent(container, new TripDays(events));
 
     const tripEventsList = container.querySelectorAll(`.trip-events__list`);
