a5a0a7f46069a81f6ffc177205735c0795fd0800
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-11-22 19:13:02 +0300

    refactor: removes sort module

--- src/mock/sort.js
+++
@@ -1,9 +0,0 @@
-import {SORT_ITEM_NAMES} from './../constants.js';
-
-export const generateSortList = () => {
-  return SORT_ITEM_NAMES.map((it) => {
-    return {
-      name: it,
-    };
-  });
-};
